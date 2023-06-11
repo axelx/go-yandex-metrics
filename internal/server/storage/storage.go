f54b217369ad335b4ad5b8f87d33e24f5b8bd725
axelx <cherniginas@edu.mos.ru>
2023-06-11 19:47:54 +0300

    iter5: правки #4

--- internal/server/storage/storage.go
+++ internal/server/storage/storage.go
@@ -35,7 +35,7 @@ func (m *MemStorage) GetMetric(typeMetric, nameMetric string) (string, error) {
 		if !t {
 			return "", err
 		}
-		return fmt.Sprintf("%.3f", v), nil
+		return fmt.Sprint(v), nil
 	case "counter":
 		v, t := m.Counter[nameMetric]
 		if !t {
