78680ba6d6e25673f6a1054b5fef7aa86ea53e12
axelx <cherniginas@edu.mos.ru>
2023-09-26 11:36:03 +0300

    increment 18

--- cmd/server/example_test.go
+++ cmd/server/example_test.go
@@ -7,93 +7,63 @@ import (
 	"net/http/httptest"
 )
 
-func ExampleUpdateJSON() {
-
+func Example() {
+	// Endpoing UpdateJSON
 	// Создаем тело запроса в виде строки
 	requestBody := []byte(`{"id": "RandomValue", "type": "gauge", "value": 0.815`)
-
 	req, err := http.NewRequest("POST", "/update/", bytes.NewBuffer(requestBody))
 	if err != nil {
 		fmt.Println(err)
 	}
 	// Создаем ResponseRecorder (реализация интерфейса http.ResponseWriter)
 	rr := httptest.NewRecorder()
-
 	// Вызываем обработчик соответствующего эндпоинта
 	handler := http.HandlerFunc(valueHandler)
 	handler.ServeHTTP(rr, req)
+	fmt.Println(rr.Code)
+	fmt.Println(rr.Body.String())
 
+	// Endpoing PING
+	req, err = http.NewRequest("POST", "/ping", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	rr = httptest.NewRecorder()
+	handler = http.HandlerFunc(pingHandler)
+	handler.ServeHTTP(rr, req)
+	fmt.Println(rr.Code)
+	fmt.Println(rr.Body.String())
+
+	// Endpoing UpdateMetric
+	req, err = http.NewRequest("POST", "/update/gauge/someMetric/33.337", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	rr = httptest.NewRecorder()
+	handler = http.HandlerFunc(updateMetricHandler)
+	handler.ServeHTTP(rr, req)
+	fmt.Println(rr.Code)
+	fmt.Println(rr.Body.String())
+
+	// Endpoing GetValue
+	requestBody = []byte(`{"id": "RandomValue", "type": "gauge"`)
+	req, err = http.NewRequest("POST", "/value/", bytes.NewBuffer(requestBody))
+	if err != nil {
+		fmt.Println(err)
+	}
+	rr = httptest.NewRecorder()
+	handler = http.HandlerFunc(valueHandler)
+	handler.ServeHTTP(rr, req)
 	fmt.Println(rr.Code)
 	fmt.Println(rr.Body.String())
 
 	// Output:
 	// 200
 	// {"id": "RandomValue", "type": "gauge", "value": 0.815}
-}
-func ExamplePing() {
-
-	req, err := http.NewRequest("POST", "/ping", nil)
-	if err != nil {
-		fmt.Println(err)
-	}
-	// Создаем ResponseRecorder (реализация интерфейса http.ResponseWriter)
-	rr := httptest.NewRecorder()
-
-	// Вызываем обработчик соответствующего эндпоинта
-	handler := http.HandlerFunc(pingHandler)
-	handler.ServeHTTP(rr, req)
-
-	fmt.Println(rr.Code)
-	fmt.Println(rr.Body.String())
-
-	//Output:
 	// 200
 	//
-}
-func ExampleUpdateMetric() {
-
-	req, err := http.NewRequest("POST", "/update/gauge/someMetric421/33.337", nil)
-	if err != nil {
-		fmt.Println(err)
-	}
-	// Создаем ResponseRecorder (реализация интерфейса http.ResponseWriter)
-	rr := httptest.NewRecorder()
-
-	// Вызываем обработчик соответствующего эндпоинта
-	handler := http.HandlerFunc(updateMetricHandler)
-	handler.ServeHTTP(rr, req)
-
-	fmt.Println(rr.Code)
-	fmt.Println(rr.Body.String())
-
-	//Output:
 	// 200
 	// 33.337
-}
-
-func ExampleValue() {
-	// Создаем тело запроса в виде строки
-	requestBody := []byte(`{"id": "RandomValue", "type": "gauge"`)
-
-	req, err := http.NewRequest("POST", "/value/", bytes.NewBuffer(requestBody))
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	// Создаем ResponseRecorder (реализация интерфейса http.ResponseWriter)
-	rr := httptest.NewRecorder()
-
-	// Вызываем обработчик соответствующего эндпоинта
-	handler := http.HandlerFunc(valueHandler)
-	handler.ServeHTTP(rr, req)
-
-	// Проверяем код ответа
-	fmt.Println(rr.Code)
-
-	// Проверяем тело ответа
-	fmt.Println(rr.Body.String())
-
-	// Output:
 	// 200
 	// {"id": "RandomValue", "type": "gauge", "value": 0.815}
 }
