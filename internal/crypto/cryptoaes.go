726bb8ace488adc64edfe341c14d64fd6b253d5c
axelx <cherniginas@edu.mos.ru>
2023-10-21 17:21:18 +0300

    increment 21 правки 6

--- internal/crypto/cryptoaes.go
+++ internal/crypto/cryptoaes.go
@@ -11,8 +11,9 @@ import (
 	"encoding/pem"
 	"fmt"
 	"io"
-	"log"
 	"os"
+
+	"github.com/axelx/go-yandex-metrics/internal/logger"
 )
 
 // EncodeRSAAES кодирование слайса байт данных с помощью публичного rsa pem ключа
@@ -24,34 +25,34 @@ func EncodeRSAAES(text []byte, fileLocationPublic string) ([]byte, error) {
 	// Генерация случайного nonce для режима шифрования GCM
 	nonce := make([]byte, 12)
 	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
-		fmt.Println("Ошибка генерации nonce:", err)
-		os.Exit(1)
+		logger.Log.Error("Crypto EncodeRSAAES", "Ошибка генерации nonce:"+err.Error())
+		return nil, err
 	}
 
 	// Загрузка открытого ключа RSA из файла
 	publicKeyData, err := os.ReadFile(fileLocationPublic)
 	if err != nil {
-		fmt.Println("Ошибка чтения открытого ключа:", err)
-		os.Exit(1)
+		logger.Log.Error("Crypto EncodeRSAAES", "Ошибка чтения открытого ключа:"+err.Error())
+		return nil, err
 	}
 	publicKey, err := parseRSAPublicKey(publicKeyData)
 	if err != nil {
-		fmt.Println("Ошибка парсинга открытого ключа:", err)
-		os.Exit(1)
+		logger.Log.Error("Crypto EncodeRSAAES", "Ошибка парсинга открытого ключа:"+err.Error())
+		return nil, err
 	}
 
 	// Шифрование AES-ключа с использованием RSA
 	encryptedKey, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, publicKey, aesKey, nil)
 	if err != nil {
-		fmt.Println("Ошибка шифрования AES-ключа:", err)
-		os.Exit(1)
+		logger.Log.Error("Crypto EncodeRSAAES", "Ошибка шифрования AES-ключа:"+err.Error())
+		return nil, err
 	}
 
 	// Шифрование строки с использованием AES
 	encryptedData, err := encryptAES([]byte(text), aesKey, nonce)
 	if err != nil {
-		fmt.Println("Ошибка шифрования данных:", err)
-		os.Exit(1)
+		logger.Log.Error("Crypto EncodeRSAAES", "Ошибка шифрования данных:"+err.Error())
+		return nil, err
 	}
 
 	// Конвертируем [][]byte в []byte
@@ -66,27 +67,27 @@ func DecodeRSAAES(d []byte, fileLocationPrivate string) ([]byte, error) {
 	// Загрузка закрытого ключа RSA из файла
 	privateKeyData, err := os.ReadFile(fileLocationPrivate)
 	if err != nil {
-		fmt.Println("Ошибка чтения закрытого ключа:", err)
-		os.Exit(1)
+		logger.Log.Error("Crypto DecodeRSAAES", "Ошибка чтения закрытого ключа:"+err.Error())
+		return nil, err
 	}
 	privateKey, err := parseRSAPrivateKey(privateKeyData)
 	if err != nil {
-		fmt.Println("Ошибка парсинга закрытого ключа:", err)
-		os.Exit(1)
+		logger.Log.Error("Crypto DecodeRSAAES", "Ошибка парсинга закрытого ключа:"+err.Error())
+		return nil, err
 	}
 
 	// Расшифровка AES-ключа с использованием RSA. data[1] = encrypted Key
 	decryptedKey, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, privateKey, data[1], nil)
 	if err != nil {
-		fmt.Println("Ошибка расшифровки AES-ключа:", err)
-		os.Exit(1)
+		logger.Log.Error("Crypto DecodeRSAAES", "Ошибка расшифровки AES-ключа:"+err.Error())
+		return nil, err
 	}
 
 	// Расшифровка строки с использованием AES
 	decryptedData, err := decryptAES(data[0], decryptedKey, data[2])
 	if err != nil {
-		fmt.Println("Ошибка расшифровки данных:", err)
-		os.Exit(1)
+		logger.Log.Error("Crypto DecodeRSAAES", "Ошибка расшифровки данных:"+err.Error())
+		return nil, err
 	}
 	return decryptedData, nil
 }
@@ -95,8 +96,8 @@ func DecodeRSAAES(d []byte, fileLocationPrivate string) ([]byte, error) {
 func generateAESKey() []byte {
 	key := make([]byte, 32)
 	if _, err := io.ReadFull(rand.Reader, key); err != nil {
-		fmt.Println("Ошибка генерации AES-ключа:", err)
-		os.Exit(1)
+		logger.Log.Error("Crypto DecodeRSAAES", "Ошибка генерации AES-ключа:"+err.Error())
+		return nil
 	}
 	return key
 }
@@ -110,7 +111,8 @@ func encryptAES(data []byte, key []byte, nonce []byte) ([]byte, error) {
 
 	aesgcm, err := cipher.NewGCM(block)
 	if err != nil {
-		return nil, fmt.Errorf("ошибка создания GCM режима для AES: %w", err)
+		logger.Log.Error("Crypto DecodeRSAAES", "ошибка создания GCM режима для AES: "+err.Error())
+		return nil, err
 	}
 
 	// Шифрование данных
@@ -126,12 +128,14 @@ func encryptAES(data []byte, key []byte, nonce []byte) ([]byte, error) {
 func decryptAES(ciphertext []byte, key []byte, nonce []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		return nil, fmt.Errorf("ошибка создания AES-шифратора: %w", err)
+		logger.Log.Error("Crypto DecodeRSAAES", "ошибка создания AES-шифратора: "+err.Error())
+		return nil, err
 	}
 
 	aesgcm, err := cipher.NewGCM(block)
 	if err != nil {
-		return nil, fmt.Errorf("ошибка создания GCM режима для AES: %w", err)
+		logger.Log.Error("Crypto DecodeRSAAES", "ошибка создания GCM режима для AES: "+err.Error())
+		return nil, err
 	}
 
 	// Извлечение nonce из зашифрованных данных
@@ -141,7 +145,8 @@ func decryptAES(ciphertext []byte, key []byte, nonce []byte) ([]byte, error) {
 	// Расшифровка данных
 	plaintext, err := aesgcm.Open(nil, realNonce, ciphertext[nonceSize:], nil)
 	if err != nil {
-		return nil, fmt.Errorf("ошибка расшифровки данных: %w", err)
+		logger.Log.Error("Crypto DecodeRSAAES", "ошибка расшифровки данных:"+err.Error())
+		return nil, err
 	}
 
 	return plaintext, nil
@@ -151,16 +156,20 @@ func decryptAES(ciphertext []byte, key []byte, nonce []byte) ([]byte, error) {
 func parseRSAPublicKey(keyData []byte) (*rsa.PublicKey, error) {
 	block, _ := pem.Decode(keyData)
 	if block == nil || block.Type != "PUBLIC KEY" {
+		logger.Log.Error("Crypto parseRSAPublicKey", "открытый ключ невалиден")
 		return nil, fmt.Errorf("открытый ключ невалиден")
 	}
 	pkey, err := x509.ParsePKIXPublicKey(block.Bytes)
 	if err != nil {
 		panic(err)
+		logger.Log.Error("Crypto parseRSAPublicKey", "ParsePKIXPublicKey: "+err.Error())
+		return nil, err
 	}
 
 	rsaKey, ok := pkey.(*rsa.PublicKey)
 	if !ok {
-		log.Fatalf("got unexpected key type: %T", pkey)
+		logger.Log.Error("Crypto", "got unexpected key type: %T")
+		return nil, err
 	}
 	return rsaKey, nil
 }
@@ -169,6 +178,7 @@ func parseRSAPublicKey(keyData []byte) (*rsa.PublicKey, error) {
 func parseRSAPrivateKey(keyData []byte) (*rsa.PrivateKey, error) {
 	block, _ := pem.Decode(keyData)
 	if block == nil || block.Type != "RSA PRIVATE KEY" {
+		logger.Log.Error("Crypto ", "закрытый ключ невалиден")
 		return nil, fmt.Errorf("закрытый ключ невалиден")
 	}
 	return x509.ParsePKCS1PrivateKey(block.Bytes)
